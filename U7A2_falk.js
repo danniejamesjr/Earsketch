51c454a72184ad33f726a51af3de60f7e2498990
Crista Falk <Crista2019@users.noreply.github.com>
2018-05-29 09:53:57 -0700

    Create U7A2_falk.js

---
+++ U7A2_falk.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//		javascript code
+//		script_name: U7A2_falk.js
+//
+//		author: Crista Falk
+//		description: U7A2 DB project
+//
+
+init();
+setTempo(120);
+
+// section 1
+fitMedia(DUBSTEP_DRUMLOOP_MAIN_001, 1, 1, 11);
+fitMedia(DUBSTEP_LEAD_019, 2, 1, 11);
+fitMedia(DUBSTEP_PERCDRUM_003, 3, 5, 11);
+fitMedia(DUBSTEP_DRUMLOOP_PART_017, 4, 9, 11);
+
+// transition
+  fitMedia(DUBSTEP_DRUMLOOP_PART_015, 1, 11, 13);
+
+// section 2
+fitMedia(DUBSTEP_BASS_WOBBLE_020, 1, 13, 21);
+fitMedia(DUBSTEP_LEAD_007, 2, 13, 21);
+
+finish();
