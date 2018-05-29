871db209ec003da7c80f88da85582df02ed7dce1
Crista Falk <Crista2019@users.noreply.github.com>
2018-05-29 09:53:12 -0700

    Create sampleScript.js

---
+++ sampleScript.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//		javascript code
+//		script_name: SampleScript.js
+//
+//		author: Crista
+//		description: learning how to use the FitMedia function
+//
+
+init();
+setTempo(120);
+
+fitMedia(TECHNO_SYNTHPLUCK_001, 1, 1, 9);
+fitMedia(DUBSTEP_DRUMLOOP_MAIN_011, 2, 1, 9);
+fitMedia(DUBSTEP_BASS_WOBBLE_039, 3, 5, 9);
+
+
+finish();
